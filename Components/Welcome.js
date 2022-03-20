1e8f8bafd6b458db27a21a6f69f1284a52e12935
Farhad Khodaei <39498931+farhadkhodaei@users.noreply.github.com>
2022-03-20 19:40:20 +0330

    Add files via upload

--- Components/Welcome.js
+++ Components/Welcome.js
@@ -5,35 +5,32 @@ class Welcome extends Component{
 
     state = {
         message : 'Welcome! Click to view JSON loaded data',
-        jss : null
+        jsdata : null
     }
-    login = () => {
+    load = () => {
         this.setState ({message :null});
         fetch('https://jsonplaceholder.typicode.com/todos/8').then(response => response.json())
-        .then(json=>this.setState({jss:json.title}));
+        .then(json=>this.setState({jsdata:json.title}));
         
 
-    }
-    lld = () => {
-        console.log(this.urll);
     }
     render(){
         return (
             <div>
-                {(!this.state.message && !this.state.jss) && <div><br/><Spinner/></div>}
-                {!this.state.jss && <h2>
+                {(!this.state.message && !this.state.jsdata) && <div><br/><Spinner/></div>}
+                {!this.state.jsdata && <h2>
                     {this.state.message}
                 </h2>}
-                {this.state.jss && 
+                {this.state.jsdata && 
                     <div>
                         <h2>The loaded data:</h2>
                         <h2 class="redstyle">
-                            {this.state.jss}
+                            {this.state.jsdata}
                         </h2>
                     </div>
                 }
-                {!this.state.jss &&
-                    <button onClick = {this.login}>
+                {!this.state.jsdata &&
+                    <button onClick = {this.load}>
                         Button
                     </button>
                 }
