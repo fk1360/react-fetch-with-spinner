a24247dc76f654b3d39a58aba9a88853fb8ee78f
Farhad Khodaei <39498931+farhadkhodaei@users.noreply.github.com>
2022-03-22 12:19:54 +0430

    Add files via upload

--- Components/Welcome.js
+++ Components/Welcome.js
@@ -4,30 +4,30 @@ import Spinner from 'reactjs-simple-spinner';
 class Welcome extends Component{
 
     state = {
-        showmessage : true,
-        jsdata : null
+        showMessage : true,
+        jsData : null
     }
     load = () => {
-        this.setState ({showmessage :false});
+        this.setState ({showMessage :false});
         fetch('https://jsonplaceholder.typicode.com/todos/8').then(response => response.json())
-        .then(json=>this.setState({jsdata:json.title}));
+        .then(json=>this.setState({jsData:json.title}));
     }
     render(){
         return (
             <div>
-                {(!this.state.showmessage && !this.state.jsdata) && <div><br/><Spinner/><br/></div>}
-                {(this.state.showmessage && !this.state.jsdata) && <h2>
+                {(!this.state.showMessage && !this.state.jsData) && <div><br/><Spinner/><br/></div>}
+                {(this.state.showMessage && !this.state.jsData) && <h2>
                     Welcome! Click to view JSON loaded data
                 </h2>}
-                {this.state.jsdata && 
+                {this.state.jsData && 
                     <div>
                         <h2>The loaded data:</h2>
                         <h2 className="redstyle">
-                            {this.state.jsdata}
+                            {this.state.jsData}
                         </h2>
                     </div>
                 }
-                {!this.state.jsdata &&
+                {!this.state.jsData &&
                     <button onClick = {this.load}>
                         Button
                     </button>
